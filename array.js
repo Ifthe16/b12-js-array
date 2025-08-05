c8180185cb1c06105bd3aefe64c1b902e3e2faad
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-06 01:21:52 +0600

    added array.js file

---
+++ array.js
@@ -0,0 +1,16 @@
+const num1 = 12;
+const num2 = 22;
+const num3 = 422;
+const num4 = 442;
+
+// (), {}, []
+
+const numbers = [12, 22, 422, 442];
+const friends = ['Abul', 'babul', 'cabul', 'dabul', 'bulbul'];
+const passed = [true, false, true, false];
+const mixedArray = [12, 'Alahbad', true, 45.566];
+
+console.log(numbers);
+console.log(friends);
+console.log(passed);
+console.log(mixedArray);
