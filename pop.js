437a61b92efc5ccd879c504ef02767211290abb5
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-07 00:17:18 +0600

    added pop.js file

---
+++ pop.js
@@ -0,0 +1,11 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+// friends.push('khailam');
+console.log(friends);
+
+
+friends.pop();
+console.log(friends);
+const out1 = friends.pop();
+console.log(out1);
+const out2 = friends.pop();
+console.log(out2);
