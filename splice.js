47340dace9e75e40ba20269a2c41808306ed611a
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-07 14:39:50 +0600

    splice.js file

---
+++ splice.js
@@ -0,0 +1,14 @@
+// Example-01
+const apps = ['insta', 'fb', 'snap', 'x', 'Youtube'];
+
+console.log(apps);
+
+apps.splice(1, 3);
+console.log(apps);
+
+// Example-02
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+
+friends.splice(2, 2, 49, true, false, 39, 0.1, 0.3, 'kailam');
+
+console.log(friends);
