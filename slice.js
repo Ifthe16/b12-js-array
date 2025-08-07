f14cfdab65630105f53c565b75dc490c7f9d98b4
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-07 14:31:24 +0600

    added slice.js file

---
+++ slice.js
@@ -0,0 +1,12 @@
+// Example-01
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'pailam'];
+console.log(friends);
+
+const newArr = friends.slice(1, 4);
+console.log(newArr);
+
+// Example-02
+const apps = ['insta', 'fb', 'snap', 'x', 'Youtube'];
+console.log(apps);
+
+console.log(apps.slice(2, 4));
